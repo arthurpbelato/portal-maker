f9181a9db2f40ee3b74eb254b9360826b17238ec
aliss <alisson.vighini@gmail.com>
2023-04-04 21:46:48 -0300

    fase dois autenticacao com usuario

--- core/src/main/java/io/tcc/core/config/security/WebSecurityConfig.java
+++ core/src/main/java/io/tcc/core/config/security/WebSecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +30,21 @@ public class WebSecurityConfig {
                 .build();
     }
 
+    @Bean
+    public UserDetailsService authorizations() {
+        User.UserBuilder users = User.builder();
+        UserDetails user = users
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
 
+       return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
