088a9e3aefd6afefa18607db82399f73f0b34c18
Arthur Poubel Belato <belatoarthur@outlook.com>
2023-04-24 14:49:01 -0300

    add main

---
+++ document-commons/src/main/java/io/tcc/documentcommons/DocumentCommonsApplication.java
@@ -0,0 +1,11 @@
+package io.tcc.documentcommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentCommonsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentCommonsApplication.class, args);
+    }
+}
