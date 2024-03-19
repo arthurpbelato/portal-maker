98f1cfbeba441b4cc4549d75ccb3c3dd07f7a4aa
arthurpbelato <belatoarthur@outlook.com>
2024-03-18 23:12:07 -0300

    remove unnecessary annotation

--- core/src/main/java/io/tcc/core/service/dto/RequestLabDTO.java
+++ core/src/main/java/io/tcc/core/service/dto/RequestLabDTO.java
@@ -3,14 +3,12 @@ package io.tcc.core.service.dto;
 import io.tcc.documentcommons.model.DocumentDTO;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@ToString
 public class RequestLabDTO {
     private String name;
     private String cpf;
