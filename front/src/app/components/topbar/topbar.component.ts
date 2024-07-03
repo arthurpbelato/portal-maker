4052c6f1da536578c77d721657575c791e4f21d1
arthurpbelato <belatoarthur@outlook.com>
2024-07-02 23:24:08 -0300

    fix topbar refresh

--- front/src/app/components/topbar/topbar.component.ts
+++ front/src/app/components/topbar/topbar.component.ts
@@ -1,12 +1,12 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
-import {MenuItem} from "primeng/api";
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {MenuItem, MessageService} from "primeng/api";
 import {LoginComponent} from "../login/login.component";
 import {UserService} from "../../../service/user.service";
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  providers: [LoginComponent],
+  providers: [LoginComponent, MessageService],
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
@@ -19,21 +19,42 @@ export class TopbarComponent implements OnInit {
   isVisible: boolean = true;
   userName: string | null = "";
 
-  constructor(private loginComponent: LoginComponent, private userService: UserService) {
-    this.userService.userRoles().subscribe(
-      resp => {
+  constructor(private loginComponent: LoginComponent,
+              private userService: UserService,
+              private messageService: MessageService) {
+    this.userService.userRoles().subscribe({
+      next: (resp: String[]) => {
         if (resp !== undefined) {
-          this.items = []
-          this.isAdmin = resp.some((role) => role === 'ROLE_ADMIN')
+          this.items = [];
+          this.isAdmin = resp.some((role) => role === 'ROLE_ADMIN');
+          this.fillMenuItemsList();
         }
       },
-      error => {
+      error: _ => {
+        this.showError();
       },
-    );
-    this.isVisible = true
+      complete: () => {
+        this.isVisible = true;
+      }
+    });
   }
 
   ngOnInit(): void {
+    this.loggedUserLoad();
+  }
+
+  loggedUserLoad(): void {
+    this.wasLogged = localStorage.getItem('token') !== null;
+    const name = localStorage.getItem('userName');
+    this.userName = name ? name : "Visitante";
+  }
+
+  logout(): void {
+    this.loginComponent.logout();
+    this.ngOnInit();
+  }
+
+  private fillMenuItemsList() {
     this.items = [
       {
         label: 'Home',
@@ -46,13 +67,6 @@ export class TopbarComponent implements OnInit {
         icon: 'pi pi-fw pi-pencil'
       }
     ];
-    this.loggedUserLoad();
-  }
-
-  loggedUserLoad() {
-    this.wasLogged = localStorage.getItem('token') !== null;
-    let name = localStorage.getItem('userName');
-    this.userName = name ? name : "Visitante";
 
     if (this.wasLogged) {
       this.items?.push(
@@ -77,13 +91,12 @@ export class TopbarComponent implements OnInit {
           label: 'Logout',
           icon: 'pi pi-fw pi-power-off',
           command: () => this.logout()
-        })
+        });
     }
   }
 
-  logout() {
-    this.loginComponent.logout();
-    this.ngOnInit();
+  showError(): void {
+    this.messageService.add({ severity: 'error', summary: 'Um erro inesperado ocorreu!', detail: 'Erro ao carregar as informações do usuário logado' });
   }
 
 }
