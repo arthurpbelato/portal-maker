0249acf43b5141aa55e5774f32c074903f8d9468
Alisson.Vighini <alisson.vighini@ridershub.com.br>
2025-06-01 18:27:13 -0300

    implement mailhog for local mail sender service

--- core/src/main/java/io/tcc/core/config/EmailConfig.java
+++ core/src/main/java/io/tcc/core/config/EmailConfig.java
@@ -1,31 +1,18 @@
 package io.tcc.core.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
-
 @Configuration
 public class EmailConfig {
 
     @Bean
+    @ConfigurationProperties(prefix = "spring.mail")
     public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("tccportalmaker@gmail.com");
-        mailSender.setPassword("sgixornounszexke");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
+        return new JavaMailSenderImpl();
     }
 
 }
